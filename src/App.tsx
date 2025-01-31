ac783f2bcb20c788296c022cc069ab88932b3b86
cxconrad <chantalconrad@kpmg.com>
2025-01-31 23:03:45 +0100

    Build of routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Header from "./layouts/Header";
-import Input from "./components/inputs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Map from "./pages/map";
+import Graph from "./pages/graph";
+import Table from "./pages/table";
 
-const Main: React.FC = () => {
+export default function App() {
   return (
-    <div>
-      <Header />
-      <Input />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/graph" element={<Graph />} />
+        <Route path="/table" element={<Table />} />
+      </Routes>
+    </Router>
   );
-};
-
-export default Main;
+}
---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../layouts/header";
+import Input from "../components/inputs";
+
+const Main = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen gap-2">
+      <Header />
+      <Input />
+    </div>
+  );
+};
+
+export default Main;
