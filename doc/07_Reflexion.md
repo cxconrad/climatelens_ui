db89904684c56da073f6068ec869fa8f198843d5
cxconrad <chantalconrad@kpmg.com>
2025-02-28 23:11:59 +0100

    app changes

---
+++ doc/07_Reflexion.md
@@ -0,0 +1,8 @@
+# Reflexion
+## Kontext
+Ziel im Rahmen dieses Projektes war die Entwicklung eines Frontend für die ClimateLens Anwendung.
+## Ziel der Relfexion:
+Mit dieser Reflexion sollen bereits kritisch hinterfrage Code-Aspekte relfektiert werden.
+
+## Heraufsforderungen und Lösungsansätze
+- Herausforderungen waren die Clean Code umsetzung über alle Dateien hinweg. Aufgrund der Anfängerprogrammier-kenntnisse wurde die Anwendung mit Hilfe von Toutorials und Debugging-Tools erstellten. 
--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,9 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        // Startseite die beim Aufruf der Anwendung angezeigt wird
         <Route path="/" element={<Home />} />
-        // Map zeigt die Wetterstationen auf der Karte an
         <Route path="/map" element={<Map />} />
-        // Plot zeigt die Wetterdaten in einem Diagramm an
         <Route path="/plot" element={<Plot />} />
-        // Table zeigt die Wetterdaten in einer Tabelle an
         <Route path="/table" element={<Table />} />
       </Routes>
     </Router>
