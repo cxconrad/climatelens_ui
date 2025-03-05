8b0db4b18f9f9fe815043669dc1498d2e6c1d1bc
cxconrad <chantalconrad@kpmg.com>
2025-03-05 22:11:02 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Die App-Komponente definiert die Routen der Anwendung
 import Home from "./pages/Home";
 import Map from "./pages/Map";
 import Plot from "./pages/Plot";
 import Table from "./pages/Table";
 
-// App-Komponente
 export default function App() {
   return (
     <Router>
